1f819f3550b219a0eb7283a0e75928e21af52e4f
Pmedinam <p.medina@sell2rent.com>
2023-11-18 02:07:25 -0500

    updating payment method

--- controllers/paymentControllers.js
+++ controllers/paymentControllers.js
@@ -2,21 +2,22 @@ import stripe from '../utils/stripe.js';
 import { logEvent } from '../utils/logger.js';
 
 export const processPayment = async (req, res) => {
-  const { amount, currency, payment_method, description } = req.body;
+  const { priceId, successUrl, failUrl } = req.body;
   try {
-
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount,
-      currency,
-      payment_method,
-      description,
-      confirm: true,
+    const paymentIntent =  await stripe.checkout.sessions.create({
+      mode: 'payment',
+      payment_method_types: ['card'],
+      line_items: [
+          {
+              price: priceId,
+              quantity: 1
+          }
+      ],
+      success_url: successUrl,
+      cancel_url: failUrl
     });
+    res.json({ success: true, sessionId: paymentIntent.url });
 
-    // Registra el evento de pago
-    // await logEvent('Payment', 'User', req.user._id);
-
-    res.json({ paymentIntent });
   } catch (error) {
     console.error('Error al procesar el pago:', error);
     res.status(500).json({ error: error.message });
